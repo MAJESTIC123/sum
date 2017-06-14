b24abd81386cfc7ce7a3ade887d8f1bac9453c66
MAJESTIC123 <karthiece996@gmail.com>
2017-06-14 09:30:22 +0530

    Create sum.java

--- sum.java
+++ sum.java
@@ -1,10 +1,10 @@
 class sum{
 public static void main(String[] args){
 int a=Integer.parseInt(args[0]);
-int b=0;
+int c=0;
 for(int i=1;i<=a;i++){
-b=i+b;}
-System.out.println("the sum  is"+b);
+c=i+c;}
+System.out.println("the sum  is"+c);
 
 }
 }
